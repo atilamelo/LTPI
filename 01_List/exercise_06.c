c18e8e3c6ca7b58f3139ad062d7e6244e8166f97
atilamelo <atilamelodesouza@gmail.com>
2022-05-31 14:56:09 -0300

    Add pointers method as requested

--- 01_List/exercise_06.c
+++ 01_List/exercise_06.c
@@ -1,29 +1,30 @@
 #include <stdio.h> 
-#define NUMBER_NUMS 3
+#define NUMBER_NUMS 10
 
 int main(){
-    int nums[NUMBER_NUMS], num_sort; 
-    int temp_sort;
+    int nums[NUMBER_NUMS], temp_sort;
+    int *p_nums = &nums[0];
 
     for(int i = 0; i < NUMBER_NUMS; i++){
-        scanf(" %d", &nums[i]);
+        scanf(" %d", p_nums+i);
     }
 
     // Method of sorting: Insertion sort  
     for(int i = 1; i < NUMBER_NUMS; i++){
-        while(i > 0 && nums[i] < nums[i-1]){
-            temp_sort = nums[i];
+        while(i > 0 && *(p_nums+i) < *(p_nums+i-1)){
+            temp_sort = *(p_nums+i);
 
             // Swap nums[i] and nums[i-1]
-            nums[i] = nums[i-1];
-            nums[i-1] = temp_sort;
+            *(p_nums+i) = nums[i-1];
+            *(p_nums+i-1) = temp_sort;
 
             i--;
         }
     }
 
+    // Print the results
     for(int i = 0; i < NUMBER_NUMS; i++){
-        printf("\n%do:%d // Adress: %p", i+1, nums[i], &nums[i]);
+        printf("\n%do:%d // Adress: %p", i+1, *(p_nums+i), p_nums+i);
     }
 
 }
